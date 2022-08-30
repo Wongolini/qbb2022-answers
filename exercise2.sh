4bf8ddba0eac2e0b7b6c9bb31fa871ec3df26f49
Nick <38267741+Wongolini@users.noreply.github.com>
2022-08-29 23:49:53 -0400

    .

--- exercise2.sh
+++
@@ -1,6 +0,0 @@
-#use flanking TSS upstream of TSS
-awk -F '\t' '{if ($4==2) print}' ~/data/bed_files/chromHMM.E116_15_coreMarks_hg38lift_stateno.chr21.bed > chromHMM.E116_15_coreMarks_hg38lift_stateno.chr21_promoters.bed
-bedtools intersect -b chromHMM.E116_15_coreMarks_hg38lift_stateno.chr21_promoters.bed -a ~/data/vcf_files/random_snippet.vcf > putative_promoters.vcf
-echo "Most likely SNP in place of Cytosine"
-grep -v "#" putative_promoters.vcf | awk '{if ($4=="C") {print $5}}' | sort | uniq -c
-
