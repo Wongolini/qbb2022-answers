20908a008d9c5971454f85cf2b9e77b9df658b2b
Nick <38267741+Wongolini@users.noreply.github.com>
2022-09-19 03:01:51 -0400

    nice

--- week2/week2_hw.py
+++ week2/week2_hw.py
@@ -72,7 +72,7 @@ class DNA_alignment(ReadIn):
     def create_f_mat(self):
         # n_rows = query length
         # n_cols = subject length
-        self.f_mat = np.zeros((len(self.query_seq),len(self.subject_seq)),dtype=np.int32)
+        self.f_mat = np.zeros((len(self.query_seq)+1,len(self.subject_seq)+1),dtype=np.int32)
         # query is rows index
         # subject is column index
         self.f_mat[:,0] = [j*self.gap_penalty for j in range(self.f_mat.shape[0])]
@@ -81,69 +81,60 @@ class DNA_alignment(ReadIn):
     
     def align(self):
         for i,q in enumerate(self.query_seq):
-            if i == 0:
-                continue
             for j,s in enumerate(self.subject_seq):
-                if j == 0:
-                    continue
                 hoxd_i = self.nucs.index(q)
                 hoxd_j = self.nucs.index(s)
-                hoxd_score = +self.f_mat[i-1,j-1]+self.score_mat[hoxd_i,hoxd_j]
-                h = self.f_mat[i,j-1] + self.gap_penalty
-                v = self.f_mat[i-1,j] + self.gap_penalty
-                self.f_mat[i,j] = max(hoxd_score,h,v)
+                hoxd_score = self.f_mat[i,j]+self.score_mat[hoxd_i,hoxd_j]
+                h = self.f_mat[i+1,j] + self.gap_penalty
+                v = self.f_mat[i,j+1] + self.gap_penalty
+                self.f_mat[i+1,j+1] = max(hoxd_score,h,v)
+
     
     def calculate_score(self):
-        traceback_mat = np.zeros((self.f_mat.shape[0], self.f_mat.shape[1]), dtype=np.int32)
-        score_list = []
-        movement_dict = {'d':[0,0], 's':[0,1],'q':[1,0]}
+        #traceback_mat = np.zeros((self.f_mat.shape[0], self.f_mat.shape[1]), dtype=np.int32)
+        movement_dict = {'d':[0,0],'q':[0,1],'s':[1,0]}
         trace = []
         i = self.f_mat.shape[0]
         j = self.f_mat.shape[1]
         movement = [i,j]
         sub_f_mat = self.f_mat[movement[0]-2:movement[0], movement[1]-2:movement[1]] # slide tensor around f_mat
 
-        while i>=1 and j>=1: # iterate upwards through rows
-            if movement[0]==0 or movement[1]==0:
-                break 
+        while movement[0]!=1 or movement[1]!=1: # iterate upwards through rows
 
-            else:
-                # decide to align or gap
-                # previous score is in (1,1) index
-                # rows are query seq
-                # columns are subject seq 
-
-                diag = sub_f_mat[0][0] # move diag
-                q_gap = sub_f_mat[1][0] # move up 
-                s_gap = sub_f_mat[0][1] # move left
-                if q_gap >= s_gap:
-                    choice = q_gap
-                    move_inds = [0,1]
-                    slide = 'q'
-                  
-                else:
-                    choice = s_gap 
-                    move_inds = [1,0]
-                    slide = 's'
-       
-                if diag>=choice: # align
-                    # move diag
-                    movement[0] = movement[0] - 1
-                    movement[1] = movement[1] - 1
-                    slide = 'd'
-                    i = i-1
-                    j = j-1
-                else: # do the gap
-                    movement[0] = movement[0] - move_inds[0]
-                    movement[1] = movement[1] - move_inds[1]
-                    i = i-movement_dict[slide][0]
-                    j = j-movement_dict[slide][1]
-                score_list.append(sub_f_mat[1,1])
-                sub_f_mat = self.f_mat[movement[0]-2:movement[0], movement[1]-2:movement[1]]
-                trace.append(slide)
+            # decide to align or gap
+            # previous score is in (1,1) index
+            # rows are query seq
+            # columns are subject seq 
+            diag = sub_f_mat[0][0] # move diag
+            q_gap = sub_f_mat[1][0] # move up 
+            s_gap = sub_f_mat[0][1] # move left
+            if q_gap >= s_gap:
+                choice = q_gap
+                move_inds = [0,1]
+                slide = 'q'
                 
-            
-        
+            else:
+                choice = s_gap 
+                move_inds = [1,0]
+                slide = 's'
+    
+            if diag>=choice: # align
+                # move diag
+                movement[0] = movement[0] - 1
+                movement[1] = movement[1] - 1
+                slide = 'd'
+                i = i-1
+                j = j-1
+            else: # do the gap
+                movement[0] = movement[0] - move_inds[0]
+                movement[1] = movement[1] - move_inds[1]
+                i = i-movement_dict[slide][0]
+                j = j-movement_dict[slide][1]
+
+            sub_f_mat = self.f_mat[movement[0]-2:movement[0], movement[1]-2:movement[1]]
+            trace.append(slide)
+        trace = np.array(trace)
+   
         seq1_ind = 0
         seq2_ind = 0
         aligned_seq1 = []
@@ -151,20 +142,20 @@ class DNA_alignment(ReadIn):
         for m in trace[::-1]:
             if m=='s':
                 aligned_seq2.append('-')
-                aligned_seq1.append(self.query_seq[seq1_ind])
                 if seq1_ind < len(self.query_seq)-1:
+                    aligned_seq1.append(self.query_seq[seq1_ind])
                     seq1_ind += 1
-            if m=='q':
+            elif m=='q':
                 aligned_seq1.append('-')
-                aligned_seq2.append(self.subject_seq[seq2_ind])
                 if seq2_ind < len(self.subject_seq)-1:
+                    aligned_seq2.append(self.subject_seq[seq2_ind])
                     seq2_ind += 1
             else:
-                aligned_seq1.append(self.query_seq[seq1_ind])
-                aligned_seq2.append(self.query_seq[seq2_ind])
                 if seq2_ind < len(self.subject_seq)-1:
+                    aligned_seq2.append(self.subject_seq[seq2_ind])
                     seq2_ind += 1
                 if seq1_ind < len(self.query_seq)-1:
+                    aligned_seq1.append(self.query_seq[seq1_ind])
                     seq1_ind += 1
         
         with open('{}.fasta'.format(self.outfile),'w') as f:
@@ -173,6 +164,7 @@ class DNA_alignment(ReadIn):
             f.write('\n>{} ALIGNED\n'.format(self.subject_id))
             f.write(''.join(aligned_seq2))
         chunks = np.arange(0,len(trace),50)
+        
         print('score',self.f_mat[self.f_mat.shape[0]-1,self.f_mat.shape[1]-1])
         print('#'*55)
         print('S1: {}'.format(self.query_id))
@@ -180,25 +172,28 @@ class DNA_alignment(ReadIn):
         print('#'*55)
         print('\n')
         for i in range(1,len(chunks)):
-
-            if i < len(trace):
-                qc = ''.join(aligned_seq1[chunks[i-1]:chunks[i]])
-                sc = ''.join(aligned_seq2[chunks[i-1]:chunks[i]])
-            else:
-                qc = ''.join(aligned_seq1[chunks[i-1]:])
-                sc = ''.join(aligned_seq2[chunks[i-1]:])
+            qc = ''.join(aligned_seq1[chunks[i-1]:chunks[i]])
+            sc = ''.join(aligned_seq2[chunks[i-1]:chunks[i]])
+            tc = trace[::-1][chunks[i-1]:chunks[i]]
+            #if i <= len(trace):
+            #    qc = ''.join(aligned_seq1[chunks[i-1]:chunks[i]])
+            #    sc = ''.join(aligned_seq2[chunks[i-1]:chunks[i]])
+            #    tc = trace[chunks[i-1]:chunks[i]]
+            #else:
+            #    qc = ''.join(aligned_seq1[chunks[i-1]:])
+            #    sc = ''.join(aligned_seq2[chunks[i-1]:])
+            #    tc = trace[chunks[i-1]:chunks[i-1]:]
 
             star = np.array((np.array(list(qc))==np.array(list(sc)))*1,dtype=object)
             lines = np.array((np.array(list(qc))==np.array(list(sc)))*1,dtype=object)
-            
+            gaps = np.where(tc=='s')[0].tolist()+np.where(tc=='q')[0].tolist()
+       
             star[np.where(star==1)[0]] = ' ' # match
             star[np.where(star==0)[0]] = '*' # mismatch
-            star[np.where(trace=='s')[0]] = '' # gap
-            star[np.where(trace=='q')[0]] = '' # gap
+            star[gaps] = ' ' # gap
             lines[np.where(lines==1)[0]] = '|'
-            lines[np.where(lines==0)[0]] = ' '
-            lines[np.where(trace=='s')[0]] = ' '
-            lines[np.where(trace=='q')[0]] = ' '
+            lines[np.where(lines==0)[0]] = '|'
+            lines[gaps] = ' '
             print("  ",''.join(star))
             print('S1',qc,chunks[i])
             print('  ', ''.join(lines))
